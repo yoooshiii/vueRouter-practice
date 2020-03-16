c82ad7ff9b3809463844a8c64f2c49e800ffd7a8
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-16 16:13:32 +0900

    restart program

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false;
+
+router.beforeEach((to,from,next)=>{
+  if(to.path === "/users/1"){
+    next("/");
+  }
+    next();
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/views/HeadNamedview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click=increment(2) name="button">+1</button>
+    <button @click=decrement(2) name="button">-1</button>
+  </div>
+</template>
+
+<script>
+// import {mapMutations} from "vuex";
+import {mapActions} from "vuex";
+
+export default {
+  methods:{
+    ...mapActions(["increment","decrement"])
+    // ...mapMutations(["increment","decrement"])
+
+    // increment(){
+    //   this.$store.commit("increment",1);
+    // },
+    // decrement(){
+    //   this.$store.commit("decrement",1);
+    // }
+  }
+}
+</script>
