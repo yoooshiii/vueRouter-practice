025fa13af56476037f8364d9ff1e159645827e35
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-30 22:18:08 +0900

    practice Constitution

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,18 @@
     <p>{{ count }}</p>
     <p>{{ double }}</p>
     <p>{{ triple }}</p>
+    <p v-if='show'>ここが変更点です</p>
+    <button @click= 'show=!show' name="button">消す</button>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+        show:true
+    }
+  },
   computed: {
     double(){
       return this.$store.getters.double;
@@ -23,3 +30,16 @@ export default {
 };
 
 </script>
+<style scoped>
+button{
+  width: 200px;
+  height: 50px;
+}
+
+.v-enter-active,.v-leave-active{
+  transition: opacity 1s;
+}
+.v-enter, .v-leave-to{
+  opacity: 0;
+}
+</style>
